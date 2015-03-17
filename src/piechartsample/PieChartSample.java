1d267b1366d8ce42989efea08724d52c7824328d
EllinooraAurasmaa <ellinoora.aurasmaa@gmail.com>
2015-03-17 12:21:30 +0200

    Ympyräkaavio
    yksinkertainen ympyräkaavio

---
+++ src/piechartsample/PieChartSample.java
@@ -0,0 +1,42 @@
+
+package piechartsample;
+
+import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.chart.*;
+import javafx.scene.Group;
+
+/**
+ *
+ * @author User
+ */
+public class PieChartSample extends Application {
+    
+    @Override public void start(Stage stage) {
+        Scene scene = new Scene(new Group());
+        stage.setTitle("Kokeilu"); //Nimeää kaavion
+        stage.setWidth(500); //asettaa leveyden
+        stage.setHeight(500);//asettaa korkeudenn
+ 
+        ObservableList<PieChart.Data> pieChartData =
+                FXCollections.observableArrayList(
+                new PieChart.Data("1", 13), //Nimeää lohkon ja asettaa määrän
+                new PieChart.Data("2", 25),
+                new PieChart.Data("3", 10),
+                new PieChart.Data("4", 22),
+                new PieChart.Data("5", 30));
+        final PieChart chart = new PieChart(pieChartData);
+        chart.setTitle("Kokeilu");
+
+        ((Group) scene.getRoot()).getChildren().add(chart);
+        stage.setScene(scene);
+        stage.show();
+    }
+ 
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
